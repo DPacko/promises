d6912132b3880d74d44b7026c99926d76d72a600
DSaquin <dawnsaq@gmail.com>
2019-01-25 06:30:44 -0800

    completed promise mini sprint

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,25 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function (err, content) {
+    if (err) {
+      callback(err, null);
+    } else {
+      var firstLine = content.split('\n');
+      callback(err, firstLine[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, function(err, res, body) {
+    if(err){
+      return callback(err, null);
+    }
+      return callback(err, res.statusCode);
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
